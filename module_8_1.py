7d38c3ae9b1cbaf8ffc289ad86894f01f864eb5b
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-11-06 00:32:29 +0800

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -5,6 +5,6 @@ def add_everything_up(a, b):
         return str(a) + str(b)
 
 
-print(add_everything_up(123.456, 'строка'))  # 123.456строка
-print(add_everything_up('яблоко', 4215))      # яблоко4215
-print(add_everything_up(123.456, 7))           # 130.456
+print(add_everything_up(123.456, 'строка'))  
+print(add_everything_up('яблоко', 4215))      
+print(add_everything_up(123.456, 7))           
