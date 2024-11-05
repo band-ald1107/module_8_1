ad6959301418029bfd24cd8b97e7bd6520152f98
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-11-06 00:31:47 +0800

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,15 +1,9 @@
 def add_everything_up(a, b):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+    try:
         return a + b
+    except TypeError:
+        return str(a) + str(b)
 
-    elif isinstance(a, str) or isinstance(b, str):
-        if type(a) is not type(b):
-            return str(a) + str(b)
-        else:
-            return a + b
-
-
-    raise TypeError("Unsupported types")
 
 print(add_everything_up(123.456, 'строка'))  # 123.456строка
 print(add_everything_up('яблоко', 4215))      # яблоко4215
